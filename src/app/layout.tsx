a6e5c1fe4fee5a54fa7a38682e3cd879e66c4933
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-06-04 12:36:38 +0600

    feat: update font family to Hind Siliguri in layout and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Hind_Siliguri } from "next/font/google";
 import "./globals.css";
 
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const hindSiliguri = Hind_Siliguri({
+  variable: "--font-hind-siliguri",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -28,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={` ${hindSiliguri.variable} antialiased`}>
         <Header />
         {children}
         <Footer />

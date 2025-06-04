03dbc39a303e4e692ff000df122a46235e0cc3a8
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-06-04 12:26:38 +0600

    Add Footer component and logo SVG to the project
    - Created a new Footer component with layout and content.
    - Imported the Footer component into the HomePage.
    - Added a logo SVG file for branding in the Footer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 
 const HomePage = () => {
   return (
     <main>
       <Header />
+      <Footer />
     </main>
   );
 };

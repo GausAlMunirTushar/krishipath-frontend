aaf28da63f8e690aa527c1bd863dea3bc5913005
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-06-06 12:16:06 +0600

    feat: update product images with correct URLs and add additional products to the product data

--- src/data/productData.ts
+++ src/data/productData.ts
@@ -1,13 +1,48 @@
 const productData = [
 	{
 		productName: "টমেটো",
-		productImage: "/images/tomatoes.jpg",
+		productImage: "https://cdn-icons-png.flaticon.com/512/135/135620.png",
 		price: 60,
 	},
 	{
 		productName: "আলু",
-		productImage: "/images/potatoes.jpg",
+		productImage: "https://cdn-icons-png.flaticon.com/512/590/590685.png",
 		price: 50,
+	},
+	{
+		productName: "পেঁয়াজ",
+		productImage: "https://cdn-icons-png.flaticon.com/512/1046/1046784.png",
+		price: 55,
+	},
+	{
+		productName: "রসুন",
+		productImage: "https://cdn-icons-png.flaticon.com/512/1046/1046792.png",
+		price: 120,
+	},
+	{
+		productName: "বেগুন",
+		productImage: "https://cdn-icons-png.flaticon.com/512/590/590685.png",
+		price: 45,
+	},
+	{
+		productName: "ধনেপাতা",
+		productImage: "https://cdn-icons-png.flaticon.com/512/3047/3047923.png",
+		price: 30,
+	},
+	{
+		productName: "শাক",
+		productImage: "https://cdn-icons-png.flaticon.com/512/1899/1899930.png",
+		price: 25,
+	},
+	{
+		productName: "মুলা",
+		productImage: "https://cdn-icons-png.flaticon.com/512/634/634881.png",
+		price: 35,
+	},
+	{
+		productName: "কপি",
+		productImage: "https://cdn-icons-png.flaticon.com/512/766/766100.png",
+		price: 40,
 	},
 ];
 

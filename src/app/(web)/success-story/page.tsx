3c6cfa00785ba88535b32f90ff6f8bdef1ddcfd5
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-06-16 23:19:07 +0600

    fix: adjust padding for success stories page layout

--- src/app/(web)/success-story/page.tsx
+++ src/app/(web)/success-story/page.tsx
@@ -3,7 +3,7 @@ import FarmerSuccessStory from "@/pages/web/success-story/FarmerSuccessStory";
 
 const FarmerSuccessStoriesPage: React.FC = () => {
 	return (
-		<div className="min-h-screen bg-gray-50 py-16 sm:py-24 lg:py-32">
+		<div className=" bg-gray-50 py-10">
 			<div className="container mx-auto px-6 lg:px-8">
 				<h2 className="text-3xl font-semibold text-green-900 text-center">
 					কৃষক সাফল্য কাহিনী

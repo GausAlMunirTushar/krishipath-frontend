c0a3b1a1d3b274568c4a7a4127eb1290e6b9f3d5
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-06-17 18:05:25 +0600

    feat: add UserDashboardPage, UserSellPage, UserSettingsPage components and integrate Header and UserMobileMenu in layout

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import UserNavbar from "@/components/layout/UserNavbar";
+import Header from "@/components/layout/Header";
+import UserMobileMenu from "@/components/layout/UserMobileMenu";
 import UserSidebar from "@/components/layout/UserSidebar";
 import { useState } from "react";
 import type { ReactNode } from "react";
@@ -16,18 +17,19 @@ export default function UserDashboardLayout({
 		<html lang="en">
 			<body className="bg-gray-50 text-gray-900">
 				<div className="flex min-h-screen flex-col lg:flex-row">
-					<UserSidebar
-						isOpen={sidebarOpen}
-						toggleSidebar={() => setSidebarOpen(!sidebarOpen)}
-					/>
-
-					<div className="flex-1 flex flex-col">
-						<UserNavbar
+					<div className="hidden lg:block">
+						<UserSidebar
+							isOpen={sidebarOpen}
 							toggleSidebar={() => setSidebarOpen(!sidebarOpen)}
 						/>
+					</div>
+
+					<div className="flex-1 flex flex-col">
+						<Header />
 						<main className="flex-1 p-4 overflow-y-auto">
 							{children}
 						</main>
+						<UserMobileMenu />
 					</div>
 				</div>
 			</body>
---
+++ src/app/user/sell/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserSellPage = () => {
+	return <div>UserSellPage</div>;
+};
+
+export default UserSellPage;
